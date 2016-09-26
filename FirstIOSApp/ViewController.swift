5ba74f8d0287ea35f30bb792fa75685ee4c18010
Matt Powley <mattpow35@gmail.com>
2016-09-26 08:55:06 -0600

    tried gui and added @IBOutlet and actions to link guy with view controller

--- FirstIOSApp/ViewController.swift
+++ FirstIOSApp/ViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
+    
+    @IBOutlet weak var firstButton: UIButton!
+    
+    @IBOutlet weak var firstLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func clickButton(sender: UIButton)
+    {
+        firstLabel.text = "you clicked the button!!"
+        firstLabel.textColor = UIColor.cyanColor()
+    }
 
 }
+
 
